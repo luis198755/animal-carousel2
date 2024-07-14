280d6614e83900a96925a1f16068b51d92bc5137
Luis Alberto <luiss_alberto@hotmail.com>
2024-07-14 09:09:09 -0600

    update

--- src/components/AnimalCarousel.js
+++ src/components/AnimalCarousel.js
@@ -92,7 +92,7 @@ const AnimalCarousel = ({ animals }) => {
         <ChevronRight className="h-6 w-6" />
       </Button>
       
-      <div className="absolute bottom-4 left-4 flex space-x-2">
+      <div className="absolute top-1 left-4 flex space-x-2">
         <Button
           variant="outline"
           className="bg-white bg-opacity-50 hover:bg-opacity-75"
@@ -111,11 +111,11 @@ const AnimalCarousel = ({ animals }) => {
         </Button>
       </div>
       
-      <div className="absolute bottom-4 left-1/2 transform -translate-x-1/2 flex space-x-2">
+      <div className="absolute bottom-6 left-1/2 transform -translate-x-1/2 flex space-x-2">
         {animals.map((_, index) => (
           <button
             key={index}
-            className={`w-3 h-3 rounded-full ${
+            className={`w-3 h-4 rounded-full ${
               index === currentIndex ? 'bg-blue-500' : 'bg-gray-300'
             }`}
             onClick={() => setCurrentIndex(index)}
@@ -124,7 +124,7 @@ const AnimalCarousel = ({ animals }) => {
         ))}
       </div>
       
-      <div className="absolute top-4 right-4 bg-white bg-opacity-50 px-2 py-1 rounded-full">
+      <div className="absolute top-2 right-4 bg-white bg-opacity-50 px-2 py-1 rounded-full">
         <span className="text-sm font-semibold">{`${currentIndex + 1} de ${animals.length}`}</span>
       </div>
     </div>
